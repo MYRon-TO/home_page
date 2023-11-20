2c1aa7fe2e7644e1242684d933314bb1698fb8b2
Myron-Mac <MYRon_TO@outlook.com>
2023-11-20 14:49:16 +0800

    Done! fixed the bug

--- src/pages/list.rs
+++ src/pages/list.rs
@@ -1,10 +1,6 @@
-use std::sync::Arc;
-
 use askama::Template;
 use axum::debug_handler;
 use axum::extract::{Path, State};
-use tokio::sync::Mutex;
-use tokio::runtime::Builder;
 use crate::db::data::SeriesDb;
 use crate::run::AppState;
 
@@ -21,29 +17,19 @@ pub struct ListTemplate {
 
 impl ListTemplate {
     pub fn new() -> Self {
-        ListTemplate { items: List{ items: Arc::new(Mutex::new(vec![])) } }
+        ListTemplate { items: List{ items: vec![] } }
     }
 
 }
 
 pub struct List{
-    pub items: Arc<Mutex<Vec<ListItem>>>,
+    pub items: Vec<ListItem>,
 }
 
 impl List{
-    pub fn get(&self) -> Vec<ListItem> {
-        let runtime = Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-        runtime.block_on(async {
-          self.items.lock().await
-        }).to_vec()
-    }
-
     /// append a item to list
     pub async fn append(&mut self, item: ListItem) {
-        self.items.lock().await.push(item);
+        self.items.push(item);
     }
 
     pub async fn deal_db_date(date: SeriesDb) -> ListItem {
@@ -95,8 +81,8 @@ impl ListItem {
 #[debug_handler]
 pub async fn list(Path(list_type): Path<String>, State(app_state): State<AppState>) -> ListTemplate {
     match list_type.as_str() {
-        // "series" => list_series( app_state ).await,
-        // "tags"   => list_tags  ( app_state ).await,
+        "series" => list_series( app_state ).await,
+        "tags"   => list_tags  ( app_state ).await,
         _        => list_all   ( app_state ).await,
     }
 }
@@ -112,24 +98,24 @@ async fn list_all(app_state: AppState) -> ListTemplate {
   list
 }
 
-// async fn list_series(_app_state: AppState) -> ListTemplate {
-//     ListTemplate {
-//         items: vec![ListItem {
-//             title: "TiTlE".to_string(),
-//             link: "lInK".to_string(),
-//             cover: "cOvEr".to_string(),
-//             desc: "dEsCrIpTiOn".to_string(),
-//         }],
-//     }
-// }
+async fn list_series(app_state: AppState) -> ListTemplate {
+  let rows = app_state.database.lock().await.get_all_series().await;
+  let mut list = ListTemplate::new();
+  for row in rows {
+    let item = List::deal_db_date(row).await;
+    list.items.append(item).await;
+  }
 
-// async fn list_tags(_app_state: AppState) -> ListTemplate {
-//     ListTemplate {
-//         items: vec![ListItem {
-//             title: "TiTlE".to_string(),
-//             link: "lInK".to_string(),
-//             cover: "cOvEr".to_string(),
-//             desc: "dEsCrIpTiOn".to_string(),
-//         }],
-//     }
-// }
+  list
+}
+
+async fn list_tags(app_state: AppState) -> ListTemplate {
+  let rows = app_state.database.lock().await.get_all_series().await;
+  let mut list = ListTemplate::new();
+  for row in rows {
+    let item = List::deal_db_date(row).await;
+    list.items.append(item).await;
+  }
+
+  list
+}
